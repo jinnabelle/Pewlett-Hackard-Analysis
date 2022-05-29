eb6089c8dbe427f37c7a9af96129dcf9c991bcd7
Jinnabelle Garce <jinnabelle.garce@gmail.com>
2022-05-29 10:13:10 -0700

    delivery 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+--Delivery 1
 --1. retrieve emp_no, first_name, last_name from employees
 SELECT emp_no,
 	first_name,
@@ -42,3 +43,39 @@ INTO retiring_titles
 FROM unique_emp
 GROUP BY title
 ORDER BY 1 DESC;
+
+--Delivery 2
+--1. employee info
+SELECT emp_no, 
+	first_name,
+	last_name,
+	birth_date
+FROM employees;
+
+--2. department info
+SELECT from_date,
+	to_date
+FROM dept_emp;
+
+--3.titles infor
+SELECT title
+FROM titles;
+
+--create mentor table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tt.title
+INTO mentorship_list
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles as tt
+ON e.emp_no = tt.emp_no
+
+WHERE de.to_date = '9999-01-01' AND
+e.birth_date between '1965-01-01' and '1965-12-31'
+ORDER BY 1 ;
